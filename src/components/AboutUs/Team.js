a4d78e6f858058a3eece7ceb398e13a1f07b010d
shantanu64 <shantanukhandelwal8@gmail.com>
2023-03-22 12:00:40 +0530

    img

--- src/components/AboutUs/Team.js
+++ src/components/AboutUs/Team.js
@@ -99,7 +99,7 @@ const Tabs = ({ color }) => {
                     <div className="rounded-xl w-48 md:w-32 lg:w-32 xl:w-48">
                       <div className="w-48 md:w-32 lg:w-32 xl:w-48 rounded-xl w-48 md:w-32 lg:w-32 xl:h-48 bg-black">
                         <img
-                          src={praduymn}
+                          src="praduymn.jpg"
                           className="h-full w-full rounded-xl"
                         />
                       </div>
